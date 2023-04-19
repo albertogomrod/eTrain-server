b149a1e4cfac773bdb2e252b029b91b841ed4f8f
Alberto Gómez <albertogomrod5@gmail.com>
2023-04-19 22:20:24 +0200

    Funcionalidad agregar nueva entrada al blog terminadaç

--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -16,7 +16,7 @@ router.get("/", isAuthenticated, async (req, res, next) => {
 // POST "/api/blog" => crear un nueva entrada de blog
 router.post("/", async (req, res, next) => {
 
-  // console.log(req.body)
+ console.log(req.body)
 
   const { title, subtitle, description, photo } = req.body
 
@@ -29,7 +29,7 @@ router.post("/", async (req, res, next) => {
       photo,
     })
 
-    // res.json(response)
+   console.log(response)
     res.status(200).json()
 
   } catch (error) {
--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -2,13 +2,13 @@ const router = require("express").Router();
 
 const uploaderBlog = require("../middlewares/cloudinary-blog.js");
 
-// POST "/api/upload/hackaton" => Sube la imagen del hackaton a crear
+// POST "/api/upload/hackaton" => Sube la imagen del blog a crear
 router.post("/blog", uploaderBlog.single("photo"), (req, res, next) => {
   if (!req.file) {
     next("No file uploaded!");
     return;
   }
-  res.json({ imageUrl: req.file.path });
+  res.json({ photo: req.file.path });
 });
 
 module.exports = router;
